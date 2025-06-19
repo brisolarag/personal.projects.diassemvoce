b21837fe37f7d3b611c6eed6ab7d6ad7c80c6228
Gabriel Ferreira Brisolara <brisolarag@gmail.com>
2025-06-19 16:40:52 +0000

    fixing build errors

--- semvoce-view/src/app/app.config.ts
+++ semvoce-view/src/app/app.config.ts
@@ -3,14 +3,12 @@ import { provideRouter } from '@angular/router';
 
 import { routes } from './app.routes';
 import { provideHttpClient, withFetch } from '@angular/common/http';
-import { provideAnimationsAsync } from '@angular/platform-browser/animations/async';
 
 export const appConfig: ApplicationConfig = {
   providers: [
     provideBrowserGlobalErrorListeners(),
     provideZoneChangeDetection({ eventCoalescing: true }),
     provideRouter(routes),
-    provideAnimationsAsync(),
     provideHttpClient(withFetch())
   ]
 };
