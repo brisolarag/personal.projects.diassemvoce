c99b33d9700193bb21ff44ea2598e578b001e780
Gabriel Ferreira Brisolara <brisolarag@gmail.com>
2025-06-22 13:56:52 +0000

    adjusting picture

--- semvoce-view/src/app/services/telegram.ts
+++ semvoce-view/src/app/services/telegram.ts
@@ -26,13 +26,11 @@ export class TelegramService {
   isLarissa() {
     const userAgent = navigator.userAgent;
     const meusUserAgents = [
-        "Mozilla/5.0 (iPhone; CPU iPhone OS 18_3_2 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/18.3.1 Mobile/15E148 Safari/604.1",
         "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Safari/537.36",
         "Mozilla/5.0 (Linux; Android 6.0; Nexus 5 Build/MRA58N) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/137.0.0.0 Mobile Safari/537.36",
         "vercel-screenshot/1.0"
     ]
     const isLarissa = !(meusUserAgents.includes(userAgent.toString()));
-    console.log(userAgent, isLarissa);
     return isLarissa;
   }
 
